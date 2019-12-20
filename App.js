c2a56d11345765714597a43467f35dee99bf11c6
Younes Henni <henni1989younes@gmail.com>
2019-12-20 21:39:12 +0000

    add aws amplify config to root component

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { createAppContainer } from 'react-navigation';
+import Amplify from 'aws-amplify';
+import awsmobile from './src/aws-exports';
 
 import { RootStack } from './src/stacks';
+
+Amplify.configure(awsmobile);
 
 export default createAppContainer(RootStack);
