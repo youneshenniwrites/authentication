9292c3010263b77a824e67e8238a8aab174f6456
Younes Henni <henni1989younes@gmail.com>
2019-12-22 13:54:17 +0000

    remove aws amplify docs still super old

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import { createAppContainer } from 'react-navigation';
-import Amplify from 'aws-amplify';
-import awsmobile from './src/aws-exports';
 
 import { RootStack } from './src/stacks';
-
-Amplify.configure(awsmobile);
 
 export default createAppContainer(RootStack);
